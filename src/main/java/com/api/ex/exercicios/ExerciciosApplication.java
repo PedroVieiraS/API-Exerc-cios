ce45a615d110eac218c5cd4710bea60bc54e4fd8
PedroVieiraS <pedrocorrea978@gmail.com>
2024-07-16 21:33:33 -0300

    estados

---
+++ src/main/java/com/api/ex/exercicios/ExerciciosApplication.java
@@ -0,0 +1,34 @@
+package com.api.ex.exercicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciosApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciosApplication.class, args);
+		
+		limpa_windows();
+
+		Estados.menu();
+
+	}
+	
+	static void limpa_windows() {
+		try {
+			if (System.getProperty("os.name").contains("Windows")) {
+				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+			} else {
+				System.out.println("Este comando só funciona no Windows.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	static void finaliza() {
+		System.out.println("\n\n\n\n");
+	}
+}
---
+++ src/test/java/com/api/ex/exercicios/ExerciciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ex.exercicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
